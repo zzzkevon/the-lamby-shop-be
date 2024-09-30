3c5ef81fe2d68d15909914200cbbedd076eaff3a
eloxng <angeloeventura@gmail.com>
2024-09-30 16:41:36 -0700

    implementation for updating a forgotten password

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const Stripe = require('stripe');
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const cors = require('cors');
+
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.post('/create-payment-intent', async (req, res) => {
   try {
@@ -17,6 +20,21 @@ app.post('/create-payment-intent', async (req, res) => {
     res.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/forgot-password-update', async (req, res) => {
+  try {
+    const { newpasshash } = req.body;
+    if (!newpasshash) 
+      return res.status(400).send('Hash is undefined');
+
+    //console.log(`Hash updated pass backend: ${newpasshash}`);
+    return res.status(200).json({message: 'Hash updated pass backend: ', hash: `${newpasshash}` })
+
+  }
+  catch(error) {
+    return res.status(500).json({message: "Error updating password backend", error: error.message});
   }
 });
 
